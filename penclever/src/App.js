5f70f8e60349c7f459ed405a616d0b5637c09f73
Ankit Ray <wonderandthinklabs@gmail.com>
2021-08-25 21:53:48 +0530

    layout initiated

--- penclever/src/App.js
+++ penclever/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {Topnav} from './components/Topnav';
+import {Viewone} from './components/View1';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+  <div>
  <Topnav/>
+ <Viewone/>
+ </div>
   );
 }
 
