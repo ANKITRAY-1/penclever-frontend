06193d7273fc640ee77a040dda271cc1bdb3cfa6
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-09-10 12:06:06 +0530

    .

--- penclever/src/App.js
+++ penclever/src/App.js
@@ -1,49 +1,54 @@
-import './App.css';
-import { Topnav } from './components/Topnav';
-import { Viewone } from './components/View1';
-import { Footer } from './components/Footer';
-import { Arcticle } from './components/Arcticle';
-import { Loder } from './components/Loder';
-import { Userprofile } from './components/Userprofile';
-import  Combine  from './components/auth/Combine';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Script.js';
-import { Switch, Route } from 'react-router';
-
+import "./App.css";
+import { Topnav } from "./components/Topnav";
+import { Viewone } from "./components/View1";
+import { Footer } from "./components/Footer";
+import { Arcticle } from "./components/Arcticle";
+import { Loder } from "./components/Loder";
+import { Userprofile } from "./components/Userprofile";
+import Combine from "./components/auth/Combine";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Script.js";
+import { Switch, Route } from "react-router";
+import Editor from "./components/Editor";
 function App() {
   return (
     <Switch>
-      <Route exact path='/'>
+      <Route exact path="/">
         <Loder />
         <Topnav />
         <Viewone />
         <Footer />
       </Route>
 
-      <Route exact path='/scince'>
+      <Route exact path="/scince">
         <Loder />
         <Topnav />
         <Footer />
       </Route>
 
-      <Route path='/indianspacestation'>
+      <Route path="/indianspacestation">
         <Loder />
         <Topnav />
         <Arcticle />
         <Footer />
       </Route>
-      <Route path='/signin'>
+      <Route path="/signin">
         <Loder />
-        <Combine/>
+        <Combine />
       </Route>
-      <Route path='/userprofile'>
-        <Loder/>
-        <Topnav/>
-       <Userprofile/>
-        <Footer/>
+      <Route path="/userprofile">
+        <Loder />
+        <Topnav />
+        <Userprofile />
+        <Footer />
+      </Route>
+      <Route path="/editor">
+        <Loder />
+        <Topnav />
+        <Editor />
+        <Footer />
       </Route>
     </Switch>
-
   );
 }
 
