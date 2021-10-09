0763b70529d4861813aa1af5d270690a95e6f691
Ankit Ray <wonderandthinklabs@gmail.com>
2021-10-09 11:05:25 +0530

    Update Maingraph.jsx

--- penclever/src/components/Maingraph.jsx
+++ penclever/src/components/Maingraph.jsx
@@ -10,25 +10,31 @@ export const Maingraph = () => {
         chartType="LineChart"
         loader={<div>Loading Chart</div>}
         data={[
-          ["x", "dogs", "cats"],
-          [0, 0, 0],
-          [1, 10, 5],
-          [2, 23, 15],
-          [3, 17, 9],
-          [4, 18, 10],
-          [5, 9, 5],
-          [6, 11, 3],
-          [7, 27, 19],
+          ["x", "View", "Likes", "Share", "Followers"],
+          [0, 0, 0, 0, 0],
+          [1 , 10, 8, 5, 1],
+          [2, 23, 15, 9, 6],
+          [3, 17, 9, 1, 8],
+          [4, 18, 10, 8, 13],
+          [5, 9, 5, 9, 8],
+          [6, 11, 3, 8, 9],
+          [7, 27, 19, 2, 18],
         ]}
         options={{
           hAxis: {
-            title: "Time",
+            title: "Date",
           },
           vAxis: {
             title: "Popularity",
           },
+          series: {
+            0: { curveType: 'function' },
+            1: { curveType: 'function' },
+            2: { curveType: 'function' },
+            3: { curveType: 'function' },
+          },
         }}
-        rootProps={{ "data-testid": "2" }}
+        rootProps={{ "data-testid": "4" }}
       />
     </div>
   );
