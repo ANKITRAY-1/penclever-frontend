3c61bfbdba8a11029ee20e936d474311094a6458
Ankit Ray <wonderandthinklabs@gmail.com>
2021-10-11 18:12:36 +0530

    .

--- penclever/src/components/Userprofile.jsx
+++ penclever/src/components/Userprofile.jsx
@@ -4,9 +4,13 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import { Sidegraph } from "./Sidegraph";
 import { Maingraph } from "./Maingraph";
+
+
 export const Userprofile = () => {
   return (
     <div>
+
+
       <Container fluid="true" style={{ padding: "10px" }}>
         <Row>
           <Col md="6" xl="7">
