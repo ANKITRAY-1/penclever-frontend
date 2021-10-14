fee4793c6d0e31308caad29da2cf6db0584fb7a2
Ankit Ray <wonderandthinklabs@gmail.com>
2021-10-14 12:51:20 +0530

    Update Userprofile.jsx

--- penclever/src/components/Userprofile.jsx
+++ penclever/src/components/Userprofile.jsx
@@ -4,12 +4,22 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import { Sidegraph } from "./Sidegraph";
 import { Maingraph } from "./Maingraph";
-
+import { Profilecard } from "./Profilecard";
+import { Profile } from "./Profile";
 
 export const Userprofile = () => {
   return (
     <div>
-
+      <Container fluid="true" style={{ padding: "10px" }}>
+        <Row>
+          <Col md="6" xl="3">
+            <Profilecard/>
+          </Col>
+          <Col md="6" xl="5">
+            <Profile/>
+          </Col>
+        </Row>
+      </Container>
 
       <Container fluid="true" style={{ padding: "10px" }}>
         <Row>
