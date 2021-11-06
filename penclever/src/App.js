7c8e3f5eb62cd35395a6b45f90d6192b3e0794cf
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-11-06 12:04:21 +0530

    .

--- penclever/src/App.js
+++ penclever/src/App.js
@@ -10,6 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./Script.js";
 import { Switch, Route } from "react-router";
 import { Editor } from "./components/Editor";
+import Tedit from "./components/Tedit.jsx";
 import PageNotFound from "./components/Error404";
 function App() {
   return (
@@ -46,7 +47,12 @@ function App() {
         <Loder />
         {/* <Topnav /> */}
         <Editor />
+
         {/* <Footer /> */}
+      </Route>
+      <Route path="/tedit">
+        <Loder />
+        <Tedit />
       </Route>
       <Route component={PageNotFound} />
     </Switch>
